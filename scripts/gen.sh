4e31455d67042f559cdbab69f522e9cdfe3e2bcf
Stanislav Jakuschevskij <stanislav.jakuschevskij@at.ibm.com>
2024-03-03 12:50:26 +0100

    chores: add boilerplate generator

---
+++ scripts/gen.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+set -o nounset
+set -o errexit
+
+folderName=$1
+fileName=$(echo $1 | sed 's/-/./g')
+
+echo $folderName
+echo $fileName
+mkdir -p src/$folderName
+touch src/$folderName/$fileName.ts || exit
+touch src/$folderName/$fileName.spec.ts || exit
+cat <<EOF > src/$folderName/README.md
+# <<ADD HEADER>>
+
+## Description
+
+tba
+
+### Example
+
+tba
+
+### Difficulty
+
+easy|medium|hard
+EOF
